6a494dc7dcd75453ad1e675dc88ff936e6e28b09
merthacioglu <mhacioglu96@gmail.com>
2024-04-01 08:10:36 +0100

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/mhacioglu/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/mhacioglu/sfgpetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package com.mhacioglu.sfgpetclinic.services;
 
 import com.mhacioglu.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
