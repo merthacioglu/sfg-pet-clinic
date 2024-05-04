c50330d80bcd7a48e97b6dcfb495db565aae0fe6
merthacioglu <mhacioglu96@gmail.com>
2024-05-04 15:35:59 +0300

    Added Spring Data JPA for PetType. Closing #43.

---
+++ pet-clinic-data/src/main/java/com/mhacioglu/sfgpetclinic/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.mhacioglu.sfgpetclinic.springdatajpa;
+
+import com.mhacioglu.sfgpetclinic.model.Pet;
+import com.mhacioglu.sfgpetclinic.repositories.PetRepository;
+import com.mhacioglu.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petHashSet = new HashSet<>();
+        petRepository.findAll().forEach(petHashSet::add);
+        return petHashSet;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
