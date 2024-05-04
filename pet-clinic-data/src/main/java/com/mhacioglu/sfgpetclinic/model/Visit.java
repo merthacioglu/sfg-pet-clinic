b84b51668d870352af731b60d11de9af448a1443
merthacioglu <mhacioglu96@gmail.com>
2024-05-04 13:17:02 +0300

    Added Spring Data JPA for Owners. Closing #40.

--- pet-clinic-data/src/main/java/com/mhacioglu/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/mhacioglu/sfgpetclinic/model/Visit.java
@@ -1,10 +1,20 @@
 package com.mhacioglu.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Table(name = "visits")
+@Entity
 public class Visit extends BaseEntity{
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @OneToOne(fetch = FetchType.EAGER)
     private Pet pet;
 
     public LocalDate getDate() {
